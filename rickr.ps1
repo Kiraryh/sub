74bedb3b3a88fea0927a22b8146e90090e3ef885
Kiraryh <recklezz91@gmail.com>
2024-12-07 15:21:54 +0100

    Update rickr.ps1

--- rickr.ps1
+++ rickr.ps1
@@ -1,1 +1,6 @@
+(New-Object -ComObject WScript.Shell).SendKeys([char]173) # Send Volume Up key 50 times
+for ($i = 0; $i -lt 50; $i++) {
+    (New-Object -ComObject WScript.Shell).SendKeys([char]175)
+}
+Write-Host "Volume set to 100% (approx)."
 Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
